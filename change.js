7e816b4c2fe3c9dbc9ae56e2a99c261a51f4da47
Kwagala Trevor <kwagalatrevorbaz@gmail.com>
2024-01-17 13:11:19 +0300

    Version8

--- change.js
+++ change.js
@@ -5,3 +5,23 @@ function generateRandomNumber() {
 
 generateRandomNumber();
 console.log("change2");
+
+let firstName = "Trevor";
+let surname = "Kwagala";
+let name = surname + " " + firstName;
+console.log(name);
+
+const objectFile = {
+  name: "Kwagala Trevor",
+  age: 23,
+};
+
+const array = ["Trevor", 23];
+
+let { first, age } = array;
+
+// console.log(array.first);
+
+['apples', 'oranges', 'grapes'].forEach((value, index) => {
+    console.log(value, index)
+})
