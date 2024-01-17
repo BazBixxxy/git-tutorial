4e70dfa65bd5f7b1b165530db95beda5b0b4c480
Baz <user@Trevors-Mac.local>
2024-01-17 09:29:13 +0300

    version 1

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hi");
+const object = {
+  name: "Baz",
+  age: 23,
+};
+
+console.log(object);
+
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
+
+greet(object.name)
---
+++ index.py
@@ -0,0 +1,9 @@
+first_name = 'Trevor'
+surname = 'Kwagala'
+
+name = surname + ' ' + first_name
+
+def greet(name) :
+    print(f'Hello {name}')
+
+greet(name)
